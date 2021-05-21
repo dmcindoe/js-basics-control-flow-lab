d56db4e35b3a70731a9343ad6d8975916fc4efc3
Andrew McIndoe <amcindoe@powerhrg.com>
2021-05-21 10:51:31 -0400

    Flow Lab

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ridelength){ 
+
+  if (ridelength <= 400){
+    return 'This one is on me!';
+}
+  if (ridelength > 2000 && ridelength <= 2500){
+  return 'I will gladly take your thirty bucks.';
+}
+  if (ridelength > 2500)
+  return 'No can do.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+ function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+
+    }
+ 
+ function switchOnCharmFromTip(tip){
+  switch (tip) {
+
+    case "generous":
+    return "Thank you so much.";
+    break
+
+    case "not as generous":
+    return "Thank you.";
+    break
+    default: 
+      return "Bye.";
+    
 }
+// => true
+ }
+
+// This Area is more or less area for testing functions above
+scuberGreetingForFeet()
